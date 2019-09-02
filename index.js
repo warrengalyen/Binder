2630b66b25c253327bc3fef390149eb41f4e28c1
Warren Galyen <wgalyen@hotmail.com>
2019-09-02 15:57:07 -0700

    Added category/subject filter and filter state prompt

--- index.js
+++ index.js
@@ -9,18 +9,32 @@ const getBinder = async book => {
 }
 const drawListBook = async () => {
     if (searchBinder.value !== '') {
+        bookContainer.innerHTML = 'Searching..'
         const data = await getBinder(searchBinder.value)
         bookContainer.innerHTML = data.items
             .map(({
                       volumeInfo
-                  }) => `<div class='book'><a href='${volumeInfo.infoLink}' target='_blank'><img class='thumbnail' src='${volumeInfo.imageLinks.thumbnail}'></a><div class='book-info'><a href='${volumeInfo.infoLink}' target='_blank'><h2 class='book-title'>${volumeInfo.title}</h2></a><div class='book-authors' onclick='updateAuthor(this);'>${volumeInfo.authors}</div></div></div>`)
+                  }) => `<div class='book'><a href='${volumeInfo.infoLink}' target='_blank'><img class='thumbnail' src='${volumeInfo.imageLinks.thumbnail}'></a><div class='book-info'><a href='${volumeInfo.infoLink}' target='_blank'><h3 class='book-title'>${volumeInfo.title}</h3></a><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");'>${volumeInfo.categories}</div></div></div>`)
             .join('')
+    } else {
+        bookContainer.innerHTML = 'Enter a search term'
     }
 }
-const updateAuthor = (e) => {
-    searchBinder.value = `inauthor:` + e.innerHTML
+const updateFilter = ({
+                          innerHTML
+                      }, f) => {
+    let m
+    switch (f) {
+        case 'author':
+            m = 'inauthor:'
+            break
+        case 'subject':
+            m = 'subject:'
+            break
+    }
+    searchBinder.value = m + innerHTML
     debounce(drawListBook, 1000)
 }
 const debounce = (fn, time, to = null) =>
     to ? clearTimeout(to) : (to = setTimeout(drawListBook, time))
-searchBinder.addEventListener('change', () => debounce(drawListBook, 1000))
+searchBinder.addEventListener('input', () => debounce(drawListBook, 1000))
