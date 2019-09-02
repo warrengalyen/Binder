a52f99f3a431c8266d34c8d11e4e115e3eb9674a
Warren Galyen <wgalyen@hotmail.com>
2019-09-02 10:32:03 -0700

    Added option to search books by author's name
    Click on an author's name to find books published by that author

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-(d => {
-    const bookContainer = d.querySelector('.list-book')
-    const searchBinder = d.querySelector('.search')
-    const getBinder = async book => {
-        const response = await fetch(
-            `https://www.googleapis.com/books/v1/volumes?q=${book}`
-        )
-        const data = await response.json()
-        return data
+const bookContainer = document.querySelector('.list-book')
+const searchBinder = document.querySelector('.search')
+const getBinder = async book => {
+    const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${book}`
+    )
+    const data = await response.json()
+    return data
+}
+const drawListBook = async () => {
+    if (searchBinder.value !== '') {
+        const data = await getBinder(searchBinder.value)
+        bookContainer.innerHTML = data.items
+            .map(({
+                      volumeInfo
+                  }) => `<div class='book'><a href='${volumeInfo.infoLink}' target='_blank'><img class='thumbnail' src='${volumeInfo.imageLinks.thumbnail}'></a><div class='book-info'><a href='${volumeInfo.infoLink}' target='_blank'><h2 class='book-title'>${volumeInfo.title}</h2></a><a class='book-authors' href='javascript:updateAuthor(this);'>${volumeInfo.authors}</a></div></div>`)
+            .join('')
     }
-    const drawListBook = async () => {
-        if (searchBinder.value !== '') {
-            const data = await getBinder(searchBinder.value)
-            bookContainer.innerHTML = data.items
-                .map(({
-                          volumeInfo
-                      }) => `<div class="book"><a href="${volumeInfo.infoLink}" target="_blank"><img class="thumbnail" src="${volumeInfo.imageLinks.thumbnail}"></a><div class="book-info"><a href="${volumeInfo.infoLink}" target="	_blank"><h2 class="book-title">${volumeInfo.title}</h2></a><div class="book-authors">${volumeInfo.authors}</div></div></div>`)
-                .join('')
-        }
-    }
-    const debounce = (fn, time, to = null) =>
-        to ? clearTimeout(to) : (to = setTimeout(drawListBook, time))
-    searchBinder.addEventListener('input', () => debounce(drawListBook, 1000))
-})(document)
+}
+const updateAuthor = (e) => {
+    document.querySelector('.search').value = `inauthor:` + e.innerHTML
+    debounce(drawListBook, 1000)
+}
+const debounce = (fn, time, to = null) =>
+    to ? clearTimeout(to) : (to = setTimeout(drawListBook, time))
+searchBinder.addEventListener('change', () => debounce(drawListBook, 1000))
